869972def71e7b09d0a155fab6b2a16a710c6291
Fernando Merim <fernando_merim@hotmail.com>
2021-07-20 13:09:26 -0300

    Implementando funções e eventos com o Java Script

---
+++ HTML/assets/js/script.js
@@ -0,0 +1,40 @@
+
+/*
+por tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementsByName()
+por Classe: getElementsByCassName()
+por seletor: querySelector()
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+assunto.style.width = '100%'
+
+function validaNome() {
+    let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length < 3) {
+        txtNome.innerHTML = 'Nome inválido'
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML = 'Nome válido'
+        txtNome.style.color = 'green'
+    }
+}
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = "E-mail inválido"
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = "E-mail válido"
+        txtEmail.style.color = 'green'
+
+
+    }
+}
