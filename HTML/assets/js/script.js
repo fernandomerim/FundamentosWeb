97d5fe36e18885f0d966b7e61a83f71d83d105d0
Fernando Merim <fernando_merim@hotmail.com>
2021-07-21 11:52:55 -0300

    Implementando verificações no formulário e zoom no mapa.

--- HTML/assets/js/script.js
+++ HTML/assets/js/script.js
@@ -10,6 +10,10 @@ por seletor: querySelector()
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk= false
+let mapa = document.querySelector('#mapa')
 
 nome.style.width = '100%'
 email.style.width = '100%'
@@ -23,6 +27,7 @@ function validaNome() {
     } else {
         txtNome.innerHTML = 'Nome válido'
         txtNome.style.color = 'green'
+        nomeOk = true
     }
 }
 
@@ -34,7 +39,41 @@ function validaEmail() {
     } else {
         txtEmail.innerHTML = "E-mail válido"
         txtEmail.style.color = 'green'
+        emailOk = true
 
 
     }
+
 }
+
+    function validaAssunto () {
+        let txtAssunto = document.querySelector('#txtAssunto')
+
+        if (assunto.value.length >= 50) {
+            txtAssunto.innerHTML = 'O limite de texto é de 500 caracteres.'
+            txtAssunto.style.color = 'red'
+            txtAssunto.style.display = 'block'
+        } else {
+            txtAssunto.style.display = 'none'
+            assuntoOk = true
+        }
+    }  
+    
+    function enviar () {
+        if (nomeOk == true && emailOk == true && assuntoOk == true) {
+            alert ('Formulário enviado com sucesso')
+        } else{
+            alert('Preencha o formulário corretamente antes de enviar.')
+        }
+
+    }
+
+    function mapaZoom (){
+        mapa.style.width = '500px'
+        mapa.style.height = '300px'
+    }
+
+    function mapaNormal (){
+        mapa.style.width = '400px'
+        mapa.style.height = '250px'
+    }
